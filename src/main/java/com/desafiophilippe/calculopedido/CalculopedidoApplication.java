f7f8d33e79e058575f6aa45599badc38476fb090
Philippe Maia <philippe.m.maia@hotmail.com>
2024-12-13 01:56:09 -0300

    Finalizando o desafio

--- src/main/java/com/desafiophilippe/calculopedido/CalculopedidoApplication.java
+++ src/main/java/com/desafiophilippe/calculopedido/CalculopedidoApplication.java
@@ -1,13 +1,37 @@
 package com.desafiophilippe.calculopedido;
 
+import com.desafiophilippe.entities.Order;
+import com.desafiophilippe.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @SpringBootApplication
-public class CalculopedidoApplication {
+@ComponentScan({"com.desafiophilippe"})
+public class CalculopedidoApplication implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculopedidoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Locale localeBr = Locale.of("pt","BR");
+		NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBr);
+
+		Order order = new Order(1034,150.00,20.0);
+		System.out.println("Pedido código " + order.getCode());
+		System.out.println("Valor total: " + dinheiro.format(orderService.total(order)));
+
+	}
 }
---
+++ src/main/java/com/desafiophilippe/services/OrderService.java
@@ -0,0 +1,17 @@
+package com.desafiophilippe.services;
+
+import com.desafiophilippe.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total(Order order){
+        return order.getBasic() - (order.getBasic() * order.getDiscount()/100)
+                + shippingService.shipment(order);
+    }
+}
