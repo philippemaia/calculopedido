af4877f5126abab574b1882b61e457def0b3486f
Philippe Maia <philippe.m.maia@hotmail.com>
2024-12-13 17:39:37 -0300

    Finalizando o desafio com ajustes finais

--- src/main/java/com/desafiophilippe/calculopedido/CalculopedidoApplication.java
+++ src/main/java/com/desafiophilippe/calculopedido/CalculopedidoApplication.java
@@ -28,9 +28,23 @@ public class CalculopedidoApplication implements CommandLineRunner {
 		Locale localeBr = Locale.of("pt","BR");
 		NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBr);
 
-		Order order = new Order(1034,150.00,20.0);
-		System.out.println("Pedido código " + order.getCode());
-		System.out.println("Valor total: " + dinheiro.format(orderService.total(order)));
+		System.out.println("------------------------------------------------------------");
+
+		Order order1 = new Order(1034,150.00,20.0);
+		System.out.println("Pedido código " + order1.getCode());
+		System.out.println("Valor total: " + dinheiro.format(orderService.total(order1)));
+
+		System.out.println("------------------------------------------------------------");
+
+		Order order2 = new Order(2282,800.00,10.0);
+		System.out.println("Pedido código " + order2.getCode());
+		System.out.println("Valor total: " + dinheiro.format(orderService.total(order2)));
+
+		System.out.println("------------------------------------------------------------");
+
+		Order order3 = new Order(1309,95.90,0.0);
+		System.out.println("Pedido código " + order3.getCode());
+		System.out.println("Valor total: " + dinheiro.format(orderService.total(order3)));
 
 	}
 }
