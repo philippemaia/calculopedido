63bb9d9cf4f377ced0b49f8f08d83fc23aa21b3c
Philippe Maia <philippe.m.maia@hotmail.com>
2024-12-13 00:49:10 -0300

    primeiro commit

---
+++ src/main/java/com/desafiophilippe/calculopedido/CalculopedidoApplication.java
@@ -0,0 +1,13 @@
+package com.desafiophilippe.calculopedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculopedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculopedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiophilippe/calculopedido/CalculopedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiophilippe.calculopedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculopedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
